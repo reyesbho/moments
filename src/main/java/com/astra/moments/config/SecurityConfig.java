ce6bca436d3f4a06420b39e0d107ccdd1144f9b5
Reyes <reyesbho@gmail.com>
2024-05-26 18:39:57 -0600

    increse sesion time

--- src/main/java/com/astra/moments/config/SecurityConfig.java
+++ src/main/java/com/astra/moments/config/SecurityConfig.java
@@ -51,16 +51,4 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:5173");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
 }
